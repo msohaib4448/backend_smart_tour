20037cf3f00a695c6b5cdcbff8cfb45926cdae0c
msohaib4448 <msohaib4448@gmail.com>
2023-02-01 23:05:18 +0500

    test

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,8 @@
 function auth(req, res, next) {
     console.log(" Routing through Auth middleware... ");
-    console.log(" Routing through Auth middleware........ ");
+    console.log(" Routing through Auth middleware 2... ");
+    console.log('ssssssssssssssssssssssss')
+
     next();
   }
   module.exports = { auth };
