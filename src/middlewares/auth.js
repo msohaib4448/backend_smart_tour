3a776f0b6a5a7d306094a807bbd3547eac14143f
msohaib4448 <msohaib4448@gmail.com>
2023-02-01 22:41:10 +0500

    middleware change line

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,7 @@
 function auth(req, res, next) {
     console.log(" Routing through Auth middleware... ");
+    console.log(" Routing through Auth middleware 2... ");
+
     next();
   }
   module.exports = { auth };
