96959b46418fde40d0aa48973b37f88923bf61f6
msohaib4448 <msohaib4448@gmail.com>
2023-02-01 23:08:56 +0500

    tea test

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,7 @@
 function auth(req, res, next) {
     console.log(" Routing through Auth middleware... ");
     console.log(" Routing through Auth middleware 2... ");
+    console.log('Tea is good for health')
 
     next();
   }
