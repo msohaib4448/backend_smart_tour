1132333aa110762655d9d4a6d95165e0a658a7b8
Nazakat341 <nazakatshehzad341@gmail.com>
2023-02-05 22:15:52 +0500

    signup

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
-const router = require("./src/routes/user");
-require("dotenv").config();
-    app.use(express.json());
-    app.use("/api/user", router);
-    const PORT = process.env.PORT;
+const userRouter = require("./src/routes/user-routes");
+
+
+
+require('dotenv').config();
+
+app.use(express.json());
+app.use("/api/users", userRouter);  
+
+
+
+
+const PORT = process.env.PORT ;
 app.listen(PORT, () => {
     console.log(`server is running at ${PORT}...`)
 })
--- src/connection/postgresql.js
+++ src/connection/postgresql.js
@@ -3,7 +3,7 @@ const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'smart_tour',
-  password: '1289',
+  password: 'postgre0308',
   port: 5432,
 })
 module.exports = { pool };
