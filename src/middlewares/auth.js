3b8fbe6eff67b2583a7e66d76f325076f6a4eacb
Nazakat Ali <88762895+Nazakat341@users.noreply.github.com>
2023-02-05 09:30:32 -0800

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,8 +1,1 @@
-function auth(req, res, next) {
-    console.log(" Routing through Auth middleware... ");
-    console.log(" Routing through Auth middleware 2... ");
-    console.log('ssssssssssssssssssssssss')
 
-    next();
-  }
-  module.exports = { auth };
