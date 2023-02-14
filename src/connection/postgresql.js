54ab780aa5be868c9f483d179a14dff61a787c1b
unknown <hafizdaniyal101@gmail.com>
2023-02-14 22:39:44 +0500

    tour pakages api

--- src/connection/postgresql.js
+++ src/connection/postgresql.js
@@ -3,13 +3,10 @@ const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'smart_tour',
-<<<<<<< HEAD
   password: '5432',
-=======
-  password: '123',
->>>>>>> f6261fa9ea5c2f4b4a3cd720791f4daebc9bb670
   port: 5432,
 })
+
 
 
 
--- src/server/routes.js
+++ src/server/routes.js
@@ -5,12 +5,13 @@ const orderRouter = require("../../src/routes/order");
 const paymentRouter = require('../routes/payment')
 const paymentverificationRouter = require('../routes/verificationpayment')
 
-module.exports=route
+// module.exports=route
 const app = require ('./expressApp');
 const bookingRouter = require('../routes/hotel-booking-routes/hotelBookingRoutes');
 const hotelRouter = require('../routes/hotel-routes/hotelRoutes');
 const roomRouter = require('../routes/room-routes/roomRoutes');
 const userRouter = require('../routes/user-routes/userRoutes')
+const packageRoutes=require('../routes/packageRoutes/packageRoutes')
 
 app.use('/hotel/', bookingRouter);
 app.use('/hotel/', hotelRouter);
@@ -20,6 +21,8 @@ app.use('/api/stripe', stripeRouter)
 app.use('/api/orders', orderRouter)
 app.use("/api/user", paymentRouter);
 app.use("/api/users", paymentverificationRouter)
+app.use("/api/tour", packageRoutes)
+
 
 
 // app.use('/api/admin/', adminRoutes);
